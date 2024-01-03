3a9cd1ddda3ea3dbf2af4cb55c80f7ffada05ae2
Rokenor <a.kovrizhnykh@gmail.com>
2024-01-04 01:28:37 +0200

    blog admin create form

--- app/dashboard/admin/blog/create/page.js
+++ app/dashboard/admin/blog/create/page.js
@@ -21,12 +21,58 @@ export default function AdminBlogCreate() {
 
   // image upload
   const uploadImage = async (e) => {
-    //
+    const file = e.target.files[0];
+    if (file) {
+      setLoading(true);
+
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", process.env.CLOUDINARY_UPLOAD_PRESET);
+
+      try {
+        const response = await fetch(process.env.CLOUDINARY_URL, {
+          method: "POST",
+          body: formData,
+        });
+
+        if (response.ok) {
+          setLoading(false);
+
+          const data = await response.json();
+          console.log("Image upload response => ", data);
+          setImage(data.secure_url);
+        }
+      } catch (err) {
+        console.error(err);
+        toast.error("Image upload failed. Try again.");
+        setLoading(false);
+      }
+    }
   };
 
   // submit to create blog api
   const handleSubmit = async (e) => {
-    //
+    try {
+      const response = await (`${process.env.API}/admin/blog`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application-json",
+        },
+        body: JSON.stringify({ title, content, category, image }),
+      });
+
+      if (response.ok) {
+        router.push("/dashboard/admin");
+        toast.success("Blog create successfully");
+      } else {
+        const dataError = await response.json();
+        toast.error(errorData.err);
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error("An error occured. Try again.");
+    }
   };
 
   // return jsx / blog create form
@@ -35,6 +81,56 @@ export default function AdminBlogCreate() {
       <div className="row">
         <div className="col">
           <p className="lead">Create Blog</p>
+
+          <label className="text-secondary">Blog title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control p-2 my-2"
+          />
+
+          <label className="text-secondary">Blog content</label>
+          <ReactQuill
+            className="border rounded my-2"
+            value={content}
+            onChange={(e) => setContent(e)}
+          />
+
+          <label className="text-secondary">Blog category</label>
+          <input
+            type="text"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="form-control p-2 my-2"
+          />
+
+          {image && (
+            <img src={image} alt="preview image" style={{ width: "100px" }} />
+          )}
+
+          <div className="d-flex justify-content-between mt-3">
+            <button className="btn btn-outline-secondary">
+              <label className="mt-2 pointer" htmlFor="upload-button">
+                {loading ? "Uploading..." : "Upload image"}
+              </label>
+              <input
+                id="upload-button"
+                type="file"
+                accept="image/*"
+                onChange={uploadImage}
+                hidden
+              />
+            </button>
+
+            <button
+              className="btn bg-primary text-light"
+              disabled={loading}
+              onClick={handleSubmit}
+            >
+              Save
+            </button>
+          </div>
         </div>
       </div>
     </div>
